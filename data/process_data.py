b4d6c940c87321eea9f19940cc2322ca11a0fa0a
engrkassar <60421672+engrkassar@users.noreply.github.com>
2020-01-29 14:07:26 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,54 @@
-import sys
+# import libraries
 
+import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return
 
 
 def clean_data(df):
-    pass
+    with open('category_names.pkl', 'rb') as f:
+        category_names = pickle.load(f)
+    categories.columns = category_names
+    df = messages.join(categories.set_index('id'), on='id')
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.get(-1)
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    df.drop(['categories'], axis=1, inplace=True)
+    df = df.join(categories, on='id')
+    df = df[df.duplicated()==False]
+
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine_write = create_engine('sqlite:///database_filename.db')
+    df.to_sql('database_filename', engine_write, index=False)
+    return  
 
 
 def main():
